b93d8065983c7a4225ec53d8fb8709537c377def
Karol Szymański <karol2134@op.pl>
2021-03-10 12:13:16 +0100

    new class settings

---
+++ settings.js
@@ -0,0 +1,7 @@
+export class {
+    constructor()
+    {
+        this.closeModalBttn = document.getElementById('closeModal');
+        this.scoreTableBttn = document.getElementById('scoreButton');
+    }
+}
