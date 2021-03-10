632fc68cffbbf16843104db08a0a912fe709f792
Karol Szymański <karol2134@op.pl>
2021-03-10 12:17:26 +0100

    setting class update

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
-export class {
+export class settings{
     constructor()
     {
         this.closeModalBttn = document.getElementById('closeModal');
         this.scoreTableBttn = document.getElementById('scoreButton');
     }
+    
 }
